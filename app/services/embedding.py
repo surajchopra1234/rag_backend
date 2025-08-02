5ca44afda0fc10b1be84ccfe3a5a3949d366ad6c
Suraj <surajchopra1603@gmail.com>
2025-08-02 23:08:43 +0530

    Refactor document loading and management functions for clarity and consistency

--- app/services/embedding.py
+++ app/services/embedding.py
@@ -1,13 +1,17 @@
 # Import necessary libraries
-from sentence_transformers import SentenceTransformer
+from app.clients.gemini_client import gemini_client
+# from sentence_transformers import SentenceTransformer
 
-# Qwen embedding model
-model = SentenceTransformer("Qwen/Qwen3-Embedding-0.6B")
+# Qwen embedding model (Local model)
+# model = SentenceTransformer("Qwen/Qwen3-Embedding-0.6B")
 
 def generate_embedding(content):
     """
     Function to generate embeddings for a content using Gemini API
     """
 
-    embeddings = model.encode(content)
-    return embeddings
+    # embeddings = model.encode(content)
+    # return embeddings
+
+    result = gemini_client.models.embed_content(model="gemini-embedding-001", contents=content)
+    return result.embeddings[0].values
